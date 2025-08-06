143b0de0f8c33c41363be08df7a3831260f4d52a
gyngxn_ <jdj0311028@naver.com>
2025-08-06 12:31:50 +0900

    session 7

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    // 수동 빈 등록이 우선권을 가짐
+    // 수동 빈이 자동 빈을 오버라이딩
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
