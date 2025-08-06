6cf3ceb10c13a05fc9b5668492056ffcb7b35d0b
gyngxn_ <jdj0311028@naver.com>
2025-08-06 22:12:32 +0900

    session 8

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,25 +1,21 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderServiceImpl implements OrderService{
+// @RequiredArgsConstructor 는 OrderServiceImpl 생성자와 같다
+public class OrderServiceImpl implements OrderService {
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // 인터페이스에만 의존 그러나 구현체가 없는데 코드가 실행이 안됨
-    // 헤결방안 - 누군가가 DiscountPolicy 구현 객체를 대신 생성후 주입 해야함
-
-    //DIP 지키고 있음 - OrderServiceImpl에만 의존
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
-
+    //생성자 자동 주입
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -28,10 +24,25 @@ public class OrderServiceImpl implements OrderService{
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+/*
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+*/
     //테스트 용도
     public MemberRepository getMemberRepository() {
         return memberRepository;
---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,39 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.bean.override.convention.TestBean;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        //@Nullable -> 호출은 되는데 null 입력
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        //자동 주입할 대상이 없으면 Optional.empty 입력
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
